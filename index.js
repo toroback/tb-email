d3207ad608ec5803cee9f0077b0e26cd2e2269b4
Cristian Jiménez <cristian.jimenez@Mac-de-Cristian.local>
2017-09-01 12:04:01 +0200

    + Cambio en lógica de carga de templates. + Templates por idioma

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 let EmailSmtp = require('./lib/email-smtp');
 let EmailImap = require('./lib/email-imap');
+var path = require('path');
+let htmlparser = require("htmlparser2"); 
+let htmlToText = require('html-to-text');
 
+const emailsDir ='/email';
+const defLang = 'en_us';
+const defLang2 = 'en';
 /*
 options ={
   user:
@@ -51,9 +57,9 @@ class Client {
   }
 
 
-  sendFromTemplate(to, templateName, replaceObject){
+  sendFromTemplate(to, templateName, replaceObject, lang){
     return new Promise((resolve,reject) => {
-       loadTemplates(templateName, replaceObject)
+       loadTemplates(templateName, replaceObject, lang)
         .then(texts =>
           this.send({
             to      : to,
@@ -119,22 +125,99 @@ class Client {
 }
 
 
-function loadTemplates(templateName, replaceObject){
-  return new Promise((resolve, reject) => {
-    let texts = { };
+function loadTemplates(templateName, replaceObject, lang = "en"){
+  return new Promise((resolve, reject) =>{
+    let texts = {};
     Promise.all([
-      App.res.loadEmail(templateName+".html", replaceObject),
-      App.res.loadEmail(templateName+".txt", replaceObject),
-      App.res.loadSubject(templateName, replaceObject)
-      ])
-      .then(resourses =>{
-         texts.html = resourses[0];
-         texts.text = resourses[1];
-         texts.subject = resourses[2];
-         resolve(texts); 
+      loadEmail(templateName, ".html", replaceObject, lang).catch(err => Promise.resolve()),
+      loadEmail(templateName, ".txt", replaceObject, lang).catch(err => Promise.resolve())
+    ])
+      .then(results =>{
+        texts.html = results[0];
+        texts.text = results[1];
+
+        if(!texts.html && !texts.text)
+          throw new Error("Cannot find resource with key: "+ templateName);
+
+        let prom = [];
+        if(texts.html){
+          prom.push(parseHtmlTitle(texts.html))
+          if(!texts.text)
+            prom.push(htmlToPlainText(texts.html));
+        }
+        
+        return Promise.all(prom);
+      })
+      .then(results =>{
+        if(results.length > 0){
+          texts.subject = results[0];
+          if(results.length > 1)
+            texts.text = results[1];
+        }
+        resolve(texts);
       })
       .catch(reject);
   });
 }
 
+function parseHtmlTitle(theHtml) {
+  return new Promise((resolve, reject) => {
+    let isTitle = false;
+    let title;
+
+    let parser = new htmlparser.Parser({
+        onopentag:  (name) => { if(name === 'title') isTitle = true; },
+        ontext:     (text) => { if (isTitle) title = text; },
+        onclosetag: (name) => { 
+          if(name === 'title') {
+            isTitle = false;
+            parser.parseComplete();
+          }
+        }
+    }, {decodeEntities: true});
+    parser.write(theHtml);
+    resolve(title);
+  });
+  
+}
+
+
+/**
+ * Carga un recurso de tipo email
+ * @param  {String} fileName El nombre del archivo incluyendo la extensión
+ * @param  {Object} params   Objeto con los parámetros del texto a reemplazar
+ * @return {String}          El recurso cargado
+ */
+function loadEmail(fileName, ext, params, lang){
+  return App.res.loadText(path.join(emailsDir, fileName, lang+ext), params)
+    .catch(err => {
+      if(lang != defLang && lang != defLang2){
+        return loadEmail(fileName, ext, params, defLang);
+      }else if (lang === defLang) {
+        return loadEmail(fileName, ext, params, defLang2);
+      }else{
+        return Promise.reject(err);
+      }
+    });
+}
+
+
+
+function htmlToPlainText(html){
+  return new Promise((resolve, reject) => {
+    var text = htmlToText.fromString(html, {
+        wordwrap: 130
+    });
+
+    resolve(text);
+  });
+}
+
+
+
 module.exports = Client;
+
+
+
+
+
